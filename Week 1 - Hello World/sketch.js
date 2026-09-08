6ecb094554635debc986ddf65378d3c8ae61e3d7
Samm-exe <nienke.sneekes@ziggo.nl>
2026-09-08 10:49:42 +0200

    Week task 1.2

--- Week 1 - Hello World/sketch.js
+++ Week 1 - Hello World/sketch.js
@@ -3,20 +3,21 @@ function setup() {
 }
 
 function draw() {
-  background(225);
+  valW = "rgb(255, 255, 255)"
+  background(valW);
   
   /*Dutch flag*/
     fill(255, 0, 0);
       rect(25, 45, 60, 15);
   
-    fill(255);
+    fill(valW);
       rect(25, 60, 60, 15);
   
     fill(0, 0, 255);
       rect(25, 75, 60, 15);
 
   /*Checkerboard*/
-    fill(255, 255, 255);
+    fill(valW);
       rect(25, 115, 60, 60);
     fill(0);
       rect(25, 115, 20, 20);
@@ -26,10 +27,16 @@ function draw() {
       rect(65, 155, 20, 20);
 
   /*House*/
-    fill(255);
+    fill(valW);
       rect(25, 300, 70, 60);
       triangle(25, 300, 95, 300, 60, 250);
   
+
+  /*Traffic light*/
+    fill(225);
+      rect(140, 50, 40, 90);
+      rect(155, 140, 10, 20);
+
   /*Teksts*/
   fill(0);
   text("1. Sam Sneekes", 10, 20);

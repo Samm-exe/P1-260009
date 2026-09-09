e28c2d21f1728218b5cab40eea90916d9f970ed2
Samm-exe <nienke.sneekes@ziggo.nl>
2026-09-09 17:35:37 +0200

    Week Task 2.2

--- Week 2 - Dynamic world/sketch.js
+++ Week 2 - Dynamic world/sketch.js
@@ -18,8 +18,6 @@ function draw() {
   rect(0, 480, 800, 490);
   fill(62, 191, 62);
   rect(0, 490, 800, 500);
-
-
   fill(150);
   rect(0, 500, 800, 600);
   stroke(255);
@@ -33,4 +31,27 @@ function draw() {
   line(610, 550, 700, 550);
   line(730, 550, 820, 550);
 
+  /*Traffic light*/
+  stroke(0)
+  strokeWeight(1);
+  fill(175);
+  rect(650, 350, 60, 100);
+  rect(665, 450, 10, 20);
+
+
+
+  /*Sun*/
+  noStroke();
+  fill(251,225,0);
+  circle(80, 80, 70);
+
+  /*Clouds*/
+  noStroke();
+  fill(255, 255, 255);
+  ellipse(240, 100, 90, 50);
+  ellipse(300, 100, 90, 40);
+  ellipse(280, 80, 50, 30);
+  ellipse(500, 130, 100, 40);
+  ellipse(550, 120, 90, 50);
+  ellipse(520, 100, 90, 50)
 }

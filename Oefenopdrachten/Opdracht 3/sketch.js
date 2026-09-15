603a098ffedd173a2c2f29a17db2d24b27aa3d89
Samm-exe <nienke.sneekes@ziggo.nl>
2026-09-15 11:22:01 +0200

    Practice task 3

--- Oefenopdrachten/Opdracht 3/sketch.js
+++ Oefenopdrachten/Opdracht 3/sketch.js
@@ -1,7 +1,34 @@
+  let score = 0
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 200);
 }
 
+function keyPressed(){
+  if (keyCode === 32) {
+    score = random(0, 100);
+  }
+}
 function draw() {
   background(220);
+  console.log(score);
+  fill(0);
+  text('Je score is', 125 , 100);
+  if (score >= 90) {
+    fill(0, 255, 0);
+    text('Uitstekend!', 200, 100);
+  }
+  else if (score >= 70 && score <= 89) {
+    fill('yellow');
+    text('Goed gedaan!', 200, 100);
+  }
+  else if (score >= 50 && score <= 69) {
+    fill('orange');
+    text('Voldoende', 200, 100);
+  }
+  else if(score <= 49) {
+    fill('red');
+    text('Onvoldoende', 200, 100);
+  }
+
 }

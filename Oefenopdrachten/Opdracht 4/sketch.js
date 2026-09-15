36d1f7d560a354b5ab0c063238d93d41136c5570
Samm-exe <nienke.sneekes@ziggo.nl>
2026-09-15 13:39:56 +0200

    Practice task 4 Finished

--- Oefenopdrachten/Opdracht 4/sketch.js
+++ Oefenopdrachten/Opdracht 4/sketch.js
@@ -1,5 +1,7 @@
 let score = 0
 let traffic_colors = 0
+let Xball = 420
+let Yball = 90
 
 function setup() {
   createCanvas(800, 400);
@@ -29,6 +31,7 @@ function DrawTrafficLight() {
 function draw() {
   background(220);
   fill(0);
+  textSize(15);
   strokeWeight(1);
   text('1. Houd B in om een blokje te laten verschijnen.', 20,20);
   text('2. Druk op spatie om het getal op 0 te zetten.', 20, 120);
@@ -65,5 +68,39 @@ function draw() {
   circle(180, 325, 25);
 
   DrawTrafficLight()
+
+  /*Eightball*/
+  textSize(50);
+  fill(0);
+  circle(Xball, Yball, 120);
+  fill(255);
+  circle(Xball, Yball -5, 90);
+  fill(0);
+  text('8', Xball - 13, Yball + 5);
+
+  if (keyIsDown(UP_ARROW) ||  keyIsDown(87)){
+    Yball = Yball - 1
+  }
+  if (keyIsDown(DOWN_ARROW) ||  keyIsDown(83)){
+    Yball = Yball + 1
+  }
+  if (keyIsDown(LEFT_ARROW) ||  keyIsDown(65)){
+    Xball = Xball - 1
+  }
+  if (keyIsDown(RIGHT_ARROW) ||  keyIsDown(68)){
+    Xball = Xball + 1
+  }
+  if (Xball >= 850) {
+    Xball = -50
+  }
+  if (Xball <= -50) {
+    Xball = 850
+  }
+  if (Yball >= 470) {
+    Yball = -50
+  }
+  if (Yball <= -50) {
+    Yball = 470
+  }
 
 }

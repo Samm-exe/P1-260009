61e0be7d915db0cef8ffd76a4f10dd8ae3e74c69
Samm-exe <nienke.sneekes@ziggo.nl>
2026-09-16 14:23:53 +0200

    Dice randomizer

---
+++ les opdracht/sketch.js
@@ -0,0 +1,60 @@
+let dice_value = 6;
+let roll_time = 0;
+
+function setup() {
+  createCanvas(400, 400);
+}
+function rolldice() { 
+  dice_value = round(random(1,6));
+}
+function draw() {
+  background(220);
+  /*Dice*/
+  fill(255);
+  strokeWeight(2);
+  rect(140, 190, 100, 100);
+
+  if (frameCount) {
+    rolldice()
+  }
+
+  if (dice_value == 1) {
+    fill(0);
+    circle(190, 240, 20);
+  }
+  if (dice_value == 2) {
+    fill(0);
+    circle(165, 215, 20);
+    circle(215, 265, 20);
+  }
+  if (dice_value == 3) {
+    fill(0);
+    circle(165, 215, 20);
+    circle(190, 240, 20);
+    circle(215, 265, 20);
+  }
+  if (dice_value == 4) {
+    fill(0);
+    circle(165, 215, 20);
+    circle(215, 215, 20);
+    circle(165, 265, 20);
+    circle(215, 265, 20);
+  }
+  if (dice_value == 5) {
+    fill(0);
+    circle(165, 215, 20);
+    circle(215, 215, 20);
+    circle(190, 240, 20);
+    circle(165, 265, 20);
+    circle(215, 265, 20);
+  }
+  if (dice_value == 6) {
+    fill(0);
+    circle(165, 215, 20);
+    circle(165, 240, 20);
+    circle(165, 265, 20);  
+    circle(215, 215, 20);
+    circle(215, 240, 20);
+    circle(215, 265, 20);
+  }
+}
